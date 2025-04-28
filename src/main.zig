d1e08345ed314ca27c6827fdad1b8ec4c7687543
James-Riordan <jamesriordan39@gmail.com>
2025-04-28 13:54:53 -0400

    .gitignore

--- src/main.zig
+++ src/main.zig
@@ -2,3 +2,5 @@ const std = @import("std");
 
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
 const lib = @import("paseto-zig_lib");
+
+pub fn main() !void {}
